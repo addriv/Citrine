681905564fb0cd535a0ba6f9390ff1e8c4da0839
addriv <adrivero89@gmail.com>
2018-01-05 20:46:27 -0500

    Update variables

--- app/services/si_conversion.rb
+++ app/services/si_conversion.rb
@@ -41,24 +41,24 @@ class SiConversion
     stack = []
     
     infix = @input_units.scan(/\W|[\w.]+/)
-    infix.each do |el|
-      if el == "("
-        stack << el
-        @converted_units += el
-      elsif el == ")"
+    infix.each do |input|
+      if input == "("
+        stack << input
+        @converted_units += input
+      elsif input == ")"
         while (popped = stack.pop) && popped != "("
           @postfix_factors << popped
         end
-        @converted_units += el
-      elsif ["/", "*"].include?(el)
+        @converted_units += input
+      elsif ["/", "*"].include?(input)
         while (last = stack.last) && last != "("
           @postfix_factors << stack.pop
         end
-        stack << el
-        @converted_units += el
+        stack << input
+        @converted_units += input
       else
-        @postfix_factors << SiConversion.convert_unit(el, :factor)
-        @converted_units += SiConversion.convert_unit(el, :unit)
+        @postfix_factors << SiConversion.convert_unit(input, :factor)
+        @converted_units += SiConversion.convert_unit(input, :unit)
       end
     end
     @postfix_factors << stack.pop while !stack.empty?
@@ -66,18 +66,18 @@ class SiConversion
 
   def convert
     i = 0
-    calc = @postfix_factors.dup
-    while calc.length > 1
-      el = calc[i]
-      if ["/", "*"].include?(el)
-        evaluated = calc[i-2].send(el.to_sym, calc[i-1].to_f)
-        calc = calc[0...i-2] + [evaluated] + calc[i+1..-1]
+    calculation = @postfix_factors.dup
+    while calculation.length > 1
+      input = calculation[i]
+      if ["/", "*"].include?(input)
+        evaluated = calculation[i-2].send(input.to_sym, calculation[i-1].to_f)
+        calculation = calculation[0...i-2] + [evaluated] + calculation[i+1..-1]
         i -= 1
       else
         i += 1
       end
     end
-    calc[0].round(14)
+    calculation[0].round(14)
   end
 end
 
